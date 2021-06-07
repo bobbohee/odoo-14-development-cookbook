bd3304e703a197ba309b0ab76a16d2a80cba05cc
bobbohee <parkbohee0705@gmail.com>
2021-06-07 22:53:14 +0900

    [ADD] Createing Odoo Add-On Modules

---
+++ local-addons/my_library/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "My Library",
+    'summary': """Manage books easily""",
+    'author': "bobbohee",
+    'category': 'Library',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        'security/groups.xml',
+        'security/ir.model.access.csv',
+        'views/library_book.xml',
+    ],
+}
